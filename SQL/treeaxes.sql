a8365b5998cf7eb0de41d3e21650e5ff8dc058d3
sebaduocccc <se.saavedrag@duocuc.cl>
2026-02-05 01:26:05 -0300

    SQL AVANCES

--- SQL/treeaxes.sql
+++ SQL/treeaxes.sql
@@ -1,8 +1,8 @@
 --- DROP
 DROP TABLE USERS CASCADE CONSTRAINTS;
 DROP TABLE ROLE CASCADE CONSTRAINTS;
-DROP TABLE publicacion CASCADE CONSTRAINTS;
-DROP TABLE mensaje CASCADE CONSTRAINTS;
+DROP TABLE PUBLICACION CASCADE CONSTRAINTS;
+-- DROP TABLE MENSAJE CASCADE CONSTRAINTS;
 
 -- OBJETOS
 
@@ -31,10 +31,12 @@ CREATE TABLE publicacion(
     id_publicacion NUMBER(5) GENERATED ALWAYS AS IDENTITY
     INCREMENT BY 1 NOT NULL,
     id_user NUMBER(8) NOT NULL,
-    content VARCHAR2(255),
+    content_pub VARCHAR2(255),
+    fecha_pub DATE DEFAULT SYSDATE NOT NULL,
     CONSTRAINT PK_PUBLICACION PRIMARY KEY (id_publicacion)
 );
 
+/*
 CREATE TABLE mensaje(
     id_mensaje NUMBER(10)GENERATED ALWAYS AS IDENTITY
     INCREMENT BY 1 NOT NULL,
@@ -42,7 +44,7 @@ CREATE TABLE mensaje(
     id_receiver NUMBER(8) NOT NULL,
     mensaje VARCHAR2(500),
     CONSTRAINT PK_MENSAJE PRIMARY KEY (id_mensaje)
-);
+);*/
 
 
 -- LLAVES FORANEAS
@@ -55,6 +57,7 @@ ALTER TABLE publicacion
     ADD CONSTRAINT FK_PUBLICACION_USER FOREIGN KEY (id_user)
         REFERENCES USERS (id_user);
 
+/*
 ALTER TABLE mensaje
     ADD CONSTRAINT FK_MENSAJE_SENDER FOREIGN KEY (id_user)
         REFERENCES USERS (id_user);
@@ -62,12 +65,13 @@ ALTER TABLE mensaje
 ALTER TABLE mensaje
     ADD CONSTRAINT FK_MENSAJE_RECEIVER FOREIGN KEY (id_user)
         REFERENCES USERS (id_user);
+        
+        */
 
 -- POBLACION
 
 -- PERMISOS === (X X X X X X)
 --              (Ver feed, Publicar en el feed, Enviar mensajes, Panel de Reportes, Panel de Moderador, Panel General de Admin)
-
 INSERT INTO ROLE (NOMBRE_ROLE,PERMISOS) VALUES ('Admin','111111');
 INSERT INTO ROLE (NOMBRE_ROLE,PERMISOS) VALUES ('Mod','111110');
 INSERT INTO ROLE (NOMBRE_ROLE,PERMISOS) VALUES ('Helper','111100');
@@ -76,9 +80,16 @@ INSERT INTO ROLE (NOMBRE_ROLE,PERMISOS) VALUES ('Muted','110000');
 INSERT INTO ROLE (NOMBRE_ROLE,PERMISOS) VALUES ('Restricted','100000');
 INSERT INTO ROLE (NOMBRE_ROLE,PERMISOS) VALUES ('VacBanned','000000');
 
-SELECT * FROM ROLE;
-
 INSERT INTO USERS(username,email,pwd,id_role) VALUES ('test','test@test.com','1234',3);
+INSERT INTO USERS(username,email,pwd,id_role) VALUES ('panxitovilla','panixo@gmail.com','12390',4);
+
+
+INSERT INTO publicacion(id_user,content_pub) VALUES (2,'Hola como esta todo el mundo');
+INSERT INTO publicacion(id_user,content_pub) VALUES (2,'ayer me levante');
+INSERT INTO publicacion(id_user,content_pub) VALUES (2,'Olaaa');
+INSERT INTO publicacion(id_user,content_pub) VALUES (2,'Adios');
+
+
 
 
 -- SELECTS
@@ -95,6 +106,14 @@ SELECT u.username NOMBRE_USER,
 FROM USERS U JOIN ROLE R
                   ON U.ID_ROLE = R.ID_ROLE;
 
+SELECT p.id_publicacion ID,
+       u.username USUARIO,
+       p.content_pub CONTENIDO,
+       p.fecha_pub FECHA_PUBLICACION
+FROM PUBLICACION P JOIN USERS U
+    ON P.ID_USER = U.ID_USER;
 
+
+SELECT * FROM PUBLICACION;
 
 COMMIT;
