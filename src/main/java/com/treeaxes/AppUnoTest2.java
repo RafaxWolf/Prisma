6eaa2626edb4a0b5a2e7395f3c760fac9bfb7a44
sebaduocccc <se.saavedrag@duocuc.cl>
2026-02-23 23:00:03 -0300

    MSG Send Complete

---
+++ src/main/java/com/treeaxes/AppUnoTest2.java
@@ -0,0 +1,33 @@
+package com.treeaxes;
+
+import com.treeaxes.CLI.MainMenu;
+import com.treeaxes.Config.AppConfig;
+import com.treeaxes.Controller.UserController;
+
+import java.util.Scanner;
+
+public class AppUnoTest2 {
+
+    /*
+     * Cargador de configuración
+     * Nota: No agregar esta misma linea en otros archivos,
+     * las agregarlo mas veces re-carga la configuracion y puede llegar a dar problemas.
+     * Es mejor importarlo.
+     * */
+    // public static final ConfigLoader cfg = new ConfigLoader();
+
+
+    public static void main(String[] args) {
+
+        UserController userController = new UserController();
+        Scanner sc = new Scanner(System.in);
+
+        AppConfig.initEnv();
+
+        String user = "panxitovilla";
+
+        MainMenu mainMenu = new MainMenu(user);
+        mainMenu.MainPage();
+
+    }
+}
