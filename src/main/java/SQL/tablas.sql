7aa2da4f7dd713220c8730488c4b1189ebd72503
sebaduocccc <se.saavedrag@duocuc.cl>
2026-03-19 20:46:57 -0300

    sampa

--- src/main/java/SQL/tablas.sql
+++ src/main/java/SQL/tablas.sql
@@ -7,3 +7,19 @@ CREATE TABLE users(
                       CONSTRAINT PK_USER PRIMARY KEY (id_user),
                       CONSTRAINT chk_is_banned CHECK (banned IN (0,1))
 );
+
+
+CREATE TABLE messages(
+
+    id_message NUMBER(8) GENERATED ALWAYS AS IDENTITY INCREMENT BY 1 NOT NULL,
+    id_user_sender NUMBER(8) NOT NULL,
+    id_user_receiver NUMBER(8) NOT NULL,
+    content_msg VARCHAR2(1000) NOT NULL,
+    fecha_mensaje DATE DEFAULT SYSDATE NOT NULL,
+
+    CONSTRAINT PK_MESSAGE PRIMARY KEY (id_message),
+
+    CONSTRAINT FK_SENDER FOREIGN KEY (id_user_sender) REFERENCES users (id_user),
+    CONSTRAINT FK_RECEIVER FOREIGN KEY (id_user_receiver) REFERENCES users (id_user)
+
+);
--- src/main/java/com/treeaxes/Prisma/Model/User.java
+++ src/main/java/com/treeaxes/Prisma/Model/User.java
@@ -18,7 +18,6 @@ public class User {
     public User() {
     }
 
-
     public int getId() {
         return id;
     }
